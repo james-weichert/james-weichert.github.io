cf28b4230d4ba65af5f0589298ea349e335e4b02
James Weichert <james.p.weichert@gmail.com>
2023-08-02 21:10:16 -0700

    Minor Changes

--- v4/scroll.js
+++ v4/scroll.js
@@ -3,6 +3,22 @@ console.log("HERE");
 
 document.body.style.setProperty('--scroll', 0);
 
+var prev_scroll = 0
+
 window.addEventListener('scroll', () => {
-    document.body.style.setProperty('--scroll', window.scrollY / (document.body.offsetHeight - window.innerHeight));
+   
+    console.log(window.scrollY);
+
+    var new_scroll = (window.scrollY / (document.body.offsetHeight - window.innerHeight)).toFixed(5);
+
+    /*if (Math.abs(new_scroll - prev_scroll) > 0.01) {
+      
+        document.body.style.setProperty('--scroll', new_scroll);
+    } */
+
+    document.body.style.setProperty('--scroll', new_scroll);
+
+    prev_scroll = new_scroll
+
   }, false);
+
